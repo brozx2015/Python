e2c028d5f1089bd0a5f1cbd06bd28dfcfe1d1fbe
zhangchao <zhangchao@ChunBo.com>
2015-11-27 11:54:33 +0800

    初始化后把仓库所有带啊全部添加

---
+++ calculate.py
@@ -0,0 +1,24 @@
+sum = 0
+#for a in list(range(101)):
+#	sum = sum + a
+#print('总和：%d' %sum)
+'''
+n = 99
+while n > 0:
+	sum = sum + n
+	n = n - 2
+
+print("奇数和：%d" %sum)
+'''
+L = ['Bart', 'Lisa', 'Adam']
+
+'''
+for x in L:
+	print("Hello,%s"%x)
+'''
+x=0
+while x < len(L):
+	print("Hello,%s"%L[int(x)])
+	x = x + 1
+
+
---
+++ if/6、if.py
@@ -0,0 +1,6 @@
+s = input('请输入你出生的年份：')
+s = int(s)
+if s<2000 :
+	print("00前")
+else :
+	print("00后")
